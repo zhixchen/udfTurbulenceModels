43eb39986916223149bf642b1ff2f319847b12a4
cz5224043 <chenzhi09@me.com>
2020-08-23 12:31:19 +0100

    First commit

---
+++ Sigma/Sigma.C
@@ -0,0 +1,191 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2019 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "Sigma.H"
+#include "fvOptions.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace LESModels
+{
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+template<class BasicTurbulenceModel>
+void Sigma<BasicTurbulenceModel>::correctNut()
+{
+   tmp<volTensorField> tgradU(fvc::grad(this->U_));
+   tensorField& gradU = tgradU.ref().primitiveFieldRef();
+   volScalarField Dsg(mag(fvc::grad(this->U_)));
+   scalarField& DsgCells = Dsg.primitiveFieldRef();
+   const cellList& cells = this->mesh().cells();
+   Switch zeroSigma = false;
+
+   forAll(cells,celli)
+   {
+      RectangularMatrix<scalar> gradUMatrix(3,3);
+      for (label ii = 0; ii<3; ii++)
+      {
+         for (label jj = 0; jj<3; jj++)
+         {
+            gradUMatrix[ii][jj] = gradU[celli].component(ii*3+jj);
+         }
+      }
+
+      DiagonalMatrix<scalar> sigmas(SVD(gradUMatrix).S());
+      scalar sigma_1(max(sigmas));
+      scalar sigma_2(sigma_1);
+      scalar sigma_3(min(sigmas));
+
+      for (label ii = 0; ii<3; ii++)
+      {
+         if(sigmas[ii]>=sigma_3 && sigmas[ii]<=sigma_1) sigma_2 = sigmas[ii];
+
+         if(sigmas[ii]<0)
+         {
+            WarningInFunction
+            << "Negative sigular values!!! sigmas = \n"
+            << sigmas <<endl;
+         }
+      }
+
+      if(sigma_1<1.e-16) zeroSigma = true;
+
+      DsgCells[celli] = sigma_3*(sigma_1-sigma_2)*(sigma_2-sigma_3)
+                        /sqr(sigma_1+1.e-16);
+   }
+
+   if(zeroSigma) WarningInFunction << "Zero sigma_1 value!!!" << endl;
+
+   this->nut_ = sqr(Csg_*this->delta())*Dsg;
+   this->nut_.correctBoundaryConditions();
+   fv::options::New(this->mesh_).correct(this->nut_);
+
+   BasicTurbulenceModel::correctNut();
+}
+
+
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+template<class BasicTurbulenceModel>
+Sigma<BasicTurbulenceModel>::Sigma
+(
+    const alphaField& alpha,
+    const rhoField& rho,
+    const volVectorField& U,
+    const surfaceScalarField& alphaRhoPhi,
+    const surfaceScalarField& phi,
+    const transportModel& transport,
+    const word& propertiesName,
+    const word& type
+)
+:
+    LESeddyViscosity<BasicTurbulenceModel>
+    (
+        type,
+        alpha,
+        rho,
+        U,
+        alphaRhoPhi,
+        phi,
+        transport,
+        propertiesName
+    ),
+
+    k_
+    (
+        IOobject
+        (
+            IOobject::groupName("k", this->U_.group()),
+            this->runTime_.timeName(),
+            this->mesh_,
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        this->mesh_,
+        dimensionedScalar ("k", dimensionSet(0,2,-2,0,0,0,0),SMALL)
+    ),
+
+    Csg_
+    (
+	dimensioned<scalar>::lookupOrAddToDict
+        (
+            "Csg",
+            this->coeffDict_,
+            1.5
+        )
+    ),
+
+   simpleFilter_(U.mesh()),
+   filterPtr_(LESfilter::New(U.mesh(), this->coeffDict())),
+   filter_(filterPtr_())
+{
+    if (type == typeName)
+    {
+        this->printCoeffs(type);
+    }
+}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+template<class BasicTurbulenceModel>
+bool Sigma<BasicTurbulenceModel>::read()
+{
+    if (LESeddyViscosity<BasicTurbulenceModel>::read())
+    {
+        Csg_.read(this->coeffDict());
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+template<class BasicTurbulenceModel>
+void Sigma<BasicTurbulenceModel>::correct()
+{
+    if (!this->turbulence_)
+    {
+        return;
+    }
+
+    LESeddyViscosity<BasicTurbulenceModel>::correct();
+    correctNut();
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace LESModels
+} // End namespace Foam
+
+// ************************************************************************* //
---
+++ lnInclude/Sigma.C
@@ -0,0 +1,1 @@
+../Sigma/Sigma.C
